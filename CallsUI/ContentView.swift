bbc8bf5a434caa57e88d3d90ad20d8aeffc4e4ec
jaesung <chic@icloud.com>
2020-01-15 21:57:51 +0900

    Initial Commit

---
+++ CallsUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CallsUI
+//
+//  Created by 이재성 on 2020/01/15.
+//  Copyright © 2020 Jaesung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
